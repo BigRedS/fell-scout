42e2ec2a938d0a9a09f8f99dbe6a2e7ba9fb000f
Avi Greenbury <ialoneambest@gmail.com>
2023-02-11 10:22:46 +0000

    Switch ignored-teams config to env vars for #2

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ export ROUTE_50mile="1 2 3 4 5 6 8 9 10 11 12 13 14 15 16 17"
 export ROUTE_50km="1 2 3 4 5 6 8 9 14 15 16 17"
 export ROUTE_30km="1 2 3 4 5 6 7 17"
 
+export IGNORE_122="on bus but not retired"
+
 cd FellScout
 
 plackup ./bin/app.psgi
